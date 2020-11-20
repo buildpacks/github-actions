ba34d06f00fed7ec193f086790067d3b3002a6ce
Ben Hale <bhale@vmware.com>
2020-11-20 09:26:28 -0800

    Toolkit
    This change introduces the toolkit package that replicates the NodeJS Github
    Action toolkit.
    
    Signed-off-by: Ben Hale <bhale@vmware.com>

---
+++ toolkit/toolkit.go
@@ -0,0 +1,259 @@
+/*
+ * Copyright 2018-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package toolkit
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+//go:generate mockery --name Toolkit --case=underscore
+
+type Toolkit interface {
+	AddPath(paths ...string) error
+	ExportVariable(name string, value string) error
+
+	GetInput(name string) (string, bool)
+	SetOutput(name string, value string)
+	GetState(name string) (string, bool)
+	SetState(name string, value string)
+	AddMask(mask string)
+
+	StartGroup(title string)
+	EndGroup()
+
+	IsDebug() bool
+	Debug(a ...interface{})
+	Debugf(format string, a ...interface{})
+	Warning(a ...interface{})
+	Warningc(context MessageContext)
+	Warningf(format string, a ...interface{})
+	Error(a ...interface{})
+	Errorc(context MessageContext)
+	Errorf(format string, a ...interface{})
+}
+
+type MessageContext struct {
+	File    string
+	Line    string
+	Column  string
+	Message string
+}
+
+func (m *MessageContext) String() string {
+	var s []string
+	if m.File != "" {
+		s = append(s, fmt.Sprintf("file=%s", m.File))
+	}
+	if m.Line != "" {
+		s = append(s, fmt.Sprintf("line=%s", m.Line))
+	}
+	if m.Column != "" {
+		s = append(s, fmt.Sprintf("col=%s", m.Column))
+	}
+
+	return fmt.Sprintf("%s::%s", strings.Join(s, ","), escape(m.Message))
+}
+
+func FailedError(a ...interface{}) error {
+	return errors.New(errorString(a...))
+}
+
+func FailedErrorc(context MessageContext) error {
+	return errors.New(errorStringc(context))
+}
+
+func FailedErrorf(format string, a ...interface{}) error {
+	return errors.New(errorStringf(format, a...))
+}
+
+type DefaultToolkit struct {
+	once sync.Once
+
+	Environment map[string]string
+	Writer      io.Writer
+}
+
+func (d *DefaultToolkit) AddPath(paths ...string) error {
+	d.once.Do(d.init)
+
+	path, ok := d.Environment["GITHUB_PATH"]
+	if !ok {
+		return fmt.Errorf("$GITHUB_PATH must be set")
+	}
+
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("unable to open %s\n%w", path, err)
+	}
+	defer f.Close()
+
+	for _, p := range paths {
+		_, _ = fmt.Fprintln(f, p)
+	}
+
+	return nil
+}
+
+func (d *DefaultToolkit) ExportVariable(name string, value string) error {
+	d.once.Do(d.init)
+
+	path, ok := d.Environment["GITHUB_ENV"]
+	if !ok {
+		return fmt.Errorf("$GITHUB_ENV must be set")
+	}
+
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("unable to open %s\n%w", path, err)
+	}
+	defer f.Close()
+
+	if strings.ContainsRune(value, '\n') {
+		if _, err := fmt.Fprintln(f, fmt.Sprintf("%s<<EOF\n%s\nEOF", name, value)); err != nil {
+			return fmt.Errorf("unable to write variable")
+		}
+	} else {
+		_, _ = fmt.Fprintln(f, fmt.Sprintf("%s=%s", name, value))
+	}
+
+	return nil
+}
+
+func (d *DefaultToolkit) GetInput(name string) (string, bool) {
+	d.once.Do(d.init)
+	s, ok := d.Environment[fmt.Sprintf("INPUT_%s", strings.ToUpper(name))]
+	return s, ok
+}
+
+func (d *DefaultToolkit) SetOutput(name string, value string) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintf(d.Writer, "::set-output name=%s::%s\n", name, escape(value))
+}
+
+func (d *DefaultToolkit) GetState(name string) (string, bool) {
+	d.once.Do(d.init)
+	s, ok := d.Environment[fmt.Sprintf("STATE_%s", strings.ToUpper(name))]
+	return s, ok
+}
+
+func (d *DefaultToolkit) SetState(name string, value string) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintf(d.Writer, "::save-state name=%s::%s\n", name, escape(value))
+}
+
+func (d *DefaultToolkit) AddMask(mask string) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintf(d.Writer, "::add-mask::%s\n", escape(mask))
+}
+
+func (d *DefaultToolkit) StartGroup(title string) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintf(d.Writer, "::group::%s\n", title)
+}
+
+func (d *DefaultToolkit) EndGroup() {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintln(d.Writer, "::endgroup::")
+}
+
+func (d *DefaultToolkit) IsDebug() bool {
+	d.once.Do(d.init)
+
+	t, err := strconv.ParseBool(d.Environment["RUNNER_DEBUG"])
+	if err != nil {
+		return false
+	}
+
+	return t
+}
+
+func (d *DefaultToolkit) Debug(a ...interface{}) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintf(d.Writer, "::debug::%s\n", escape(fmt.Sprint(a...)))
+}
+
+func (d *DefaultToolkit) Debugf(format string, a ...interface{}) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintf(d.Writer, "::debug::%s\n", escape(fmt.Sprintf(format, a...)))
+}
+
+func (d *DefaultToolkit) Error(a ...interface{}) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintln(d.Writer, errorString(a...))
+}
+
+func (d *DefaultToolkit) Errorc(context MessageContext) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintln(d.Writer, errorStringc(context))
+}
+
+func (d *DefaultToolkit) Errorf(format string, a ...interface{}) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintln(d.Writer, errorStringf(format, a...))
+}
+
+func (d *DefaultToolkit) Warning(a ...interface{}) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintf(d.Writer, "::warning ::%s\n", escape(fmt.Sprint(a...)))
+}
+
+func (d *DefaultToolkit) Warningc(context MessageContext) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintln(d.Writer, "::warning", escape(context.String()))
+}
+
+func (d *DefaultToolkit) Warningf(format string, a ...interface{}) {
+	d.once.Do(d.init)
+	_, _ = fmt.Fprintf(d.Writer, "::warning ::%s\n", escape(fmt.Sprintf(format, a...)))
+}
+
+func (d *DefaultToolkit) init() {
+	if d.Environment == nil {
+		d.Environment = make(map[string]string)
+
+		for _, s := range os.Environ() {
+			t := strings.SplitN(s, "=", 2)
+			d.Environment[t[0]] = t[1]
+		}
+	}
+
+	if d.Writer == nil {
+		d.Writer = os.Stdout
+	}
+}
+
+func errorString(a ...interface{}) string {
+	return fmt.Sprintf("::error ::%s", escape(fmt.Sprint(a...)))
+}
+
+func errorStringc(context MessageContext) string {
+	return fmt.Sprintf("::error %s", context.String())
+}
+
+func errorStringf(format string, a ...interface{}) string {
+	return fmt.Sprintf("::error ::%s", escape(fmt.Errorf(format, a...).Error()))
+}
+
+func escape(s string) string {
+	return strings.ReplaceAll(s, "\n", "%0A")
+}
---
+++ toolkit/toolkit_test.go
@@ -0,0 +1,241 @@
+/*
+ * Copyright 2018-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package toolkit_test
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+
+	"github.com/buildpacks/github-actions/toolkit"
+)
+
+func TestToolkit(t *testing.T) {
+	spec.Run(t, "Toolkit", func(t *testing.T, context spec.G, it spec.S) {
+		var (
+			Expect = NewWithT(t).Expect
+		)
+
+		context("MessageContext", func() {
+
+			it("renders string", func() {
+				mc := toolkit.MessageContext{Message: "test-message-1\ntest-message-2"}
+				Expect(mc.String()).To(Equal("::test-message-1%0Atest-message-2"))
+
+				mc.File = "test-file"
+				Expect(mc.String()).To(Equal("file=test-file::test-message-1%0Atest-message-2"))
+
+				mc.Line = "test-line"
+				Expect(mc.String()).To(Equal("file=test-file,line=test-line::test-message-1%0Atest-message-2"))
+
+				mc.Column = "test-column"
+				Expect(mc.String()).To(Equal("file=test-file,line=test-line,col=test-column::test-message-1%0Atest-message-2"))
+			})
+
+		})
+
+		context("FailedError", func() {
+
+			it("returns failed error", func() {
+				Expect(toolkit.FailedError("test-message-1", "test-message-2\ntest-message-3")).
+					To(MatchError("::error ::test-message-1test-message-2%0Atest-message-3"))
+			})
+
+			it("returns failed errorc", func() {
+				Expect(toolkit.FailedErrorc(toolkit.MessageContext{
+					File:    "test-file",
+					Line:    "test-line",
+					Column:  "test-column",
+					Message: "test-message-1 test-message-2\ntest-message-3",
+				})).
+					To(MatchError("::error file=test-file,line=test-line,col=test-column::test-message-1 test-message-2%0Atest-message-3"))
+			})
+
+			it("returns failed errorf", func() {
+				Expect(toolkit.FailedErrorf("%s %s\n%s", "test-message-1", "test-message-2", "test-message-3")).
+					To(MatchError("::error ::test-message-1 test-message-2%0Atest-message-3"))
+			})
+
+		})
+
+		context("DefaultToolkit", func() {
+
+			var (
+				b  = &bytes.Buffer{}
+				tk = toolkit.DefaultToolkit{Writer: b}
+			)
+
+			it("adds path", func() {
+				f, err := ioutil.TempFile("", "github-path")
+				Expect(err).NotTo(HaveOccurred())
+				_, err = fmt.Fprintln(f, "test-value")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(f.Close()).To(Succeed())
+
+				tk.Environment = map[string]string{"GITHUB_PATH": f.Name()}
+
+				Expect(tk.AddPath("test-path-1", "test-path-2")).To(Succeed())
+
+				b, err := ioutil.ReadFile(f.Name())
+				Expect(string(b)).To(Equal("test-value\ntest-path-1\ntest-path-2\n"))
+			})
+
+			it("exports variable", func() {
+				f, err := ioutil.TempFile("", "github-env")
+				Expect(err).NotTo(HaveOccurred())
+				_, err = fmt.Fprintln(f, "TEST_KEY=test-value")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(f.Close()).To(Succeed())
+
+				tk.Environment = map[string]string{"GITHUB_ENV": f.Name()}
+
+				Expect(tk.ExportVariable("TEST_NAME_1", "test-value-1")).To(Succeed())
+				Expect(tk.ExportVariable("TEST_NAME_2", "test-value-2\ntest-value-3")).To(Succeed())
+
+				b, err := ioutil.ReadFile(f.Name())
+				Expect(string(b)).To(Equal("TEST_KEY=test-value\nTEST_NAME_1=test-value-1\nTEST_NAME_2<<EOF\ntest-value-2\ntest-value-3\nEOF\n"))
+			})
+
+			it("gets input", func() {
+				tk.Environment = map[string]string{"INPUT_TEST-1": "test-value"}
+
+				s, ok := tk.GetInput("test-1")
+				Expect(ok).To(BeTrue())
+				Expect(s).To(Equal("test-value"))
+
+				_, ok = tk.GetInput("test-2")
+				Expect(ok).To(BeFalse())
+			})
+
+			it("sets output", func() {
+				tk.SetOutput("test-name", "test-value-1\ntest-value-2")
+
+				Expect(b.String()).To(Equal("::set-output name=test-name::test-value-1%0Atest-value-2\n"))
+			})
+
+			it("gets state", func() {
+				tk.Environment = map[string]string{"STATE_TEST-1": "test-value"}
+
+				s, ok := tk.GetState("test-1")
+				Expect(ok).To(BeTrue())
+				Expect(s).To(Equal("test-value"))
+
+				_, ok = tk.GetState("test-2")
+				Expect(ok).To(BeFalse())
+			})
+
+			it("sets state", func() {
+				tk.SetState("test-name", "test-value-1\ntest-value-2")
+
+				Expect(b.String()).To(Equal("::save-state name=test-name::test-value-1%0Atest-value-2\n"))
+			})
+
+			it("adds mask", func() {
+				tk.AddMask("test-mask")
+
+				Expect(b.String()).To(Equal("::add-mask::test-mask\n"))
+			})
+
+			it("starts group", func() {
+				tk.StartGroup("test-title")
+
+				Expect(b.String()).To(Equal("::group::test-title\n"))
+			})
+
+			it("ends group", func() {
+				tk.EndGroup()
+
+				Expect(b.String()).To(Equal("::endgroup::\n"))
+			})
+
+			it("returns isDebug", func() {
+				tk.Environment = map[string]string{}
+
+				Expect(tk.IsDebug()).To(BeFalse())
+
+				tk.Environment["RUNNER_DEBUG"] = "true"
+
+				Expect(tk.IsDebug()).To(BeTrue())
+			})
+
+			it("writes debug", func() {
+				tk.Debug("test-message-1", "test-message-2\ntest-message-3")
+
+				Expect(b.String()).To(Equal("::debug::test-message-1test-message-2%0Atest-message-3\n"))
+			})
+
+			it("writes debugf", func() {
+				tk.Debugf("%s %s\n%s", "test-message-1", "test-message-2", "test-message-3")
+
+				Expect(b.String()).To(Equal("::debug::test-message-1 test-message-2%0Atest-message-3\n"))
+			})
+
+			it("writes warning", func() {
+				tk.Warning("test-message-1", "test-message-2\ntest-message-3")
+
+				Expect(b.String()).To(Equal("::warning ::test-message-1test-message-2%0Atest-message-3\n"))
+			})
+
+			it("writes warningc", func() {
+				tk.Warningc(toolkit.MessageContext{
+					File:    "test-file",
+					Line:    "test-line",
+					Column:  "test-column",
+					Message: "test-message-1 test-message-2\ntest-message-3",
+				})
+
+				Expect(b.String()).To(Equal("::warning file=test-file,line=test-line,col=test-column::test-message-1 test-message-2%0Atest-message-3\n"))
+			})
+
+			it("writes warningf", func() {
+				tk.Warningf("%s %s\n%s", "test-message-1", "test-message-2", "test-message-3")
+
+				Expect(b.String()).To(Equal("::warning ::test-message-1 test-message-2%0Atest-message-3\n"))
+			})
+
+			it("writes error", func() {
+				tk.Error("test-message-1", "test-message-2\ntest-message-3")
+
+				Expect(b.String()).To(Equal("::error ::test-message-1test-message-2%0Atest-message-3\n"))
+			})
+
+			it("writes errorc", func() {
+				tk.Errorc(toolkit.MessageContext{
+					File:    "test-file",
+					Line:    "test-line",
+					Column:  "test-column",
+					Message: "test-message-1 test-message-2\ntest-message-3",
+				})
+
+				Expect(b.String()).To(Equal("::error file=test-file,line=test-line,col=test-column::test-message-1 test-message-2%0Atest-message-3\n"))
+			})
+
+			it("writes errorf", func() {
+				tk.Errorf("%s %s\n%s", "test-message-1", "test-message-2", "test-message-3")
+
+				Expect(b.String()).To(Equal("::error ::test-message-1 test-message-2%0Atest-message-3\n"))
+			})
+
+		})
+
+	}, spec.Report(report.Terminal{}))
+}
